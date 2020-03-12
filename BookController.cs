d251f708a6428057ba528e644c04bdb4dfd1e3e3
raymark011 <45749475+raymark011@users.noreply.github.com>
2020-03-13 04:07:25 +0800

    Add files via upload

---
+++ BookController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCtest.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult Index()
+        {
+  
+                var book = new MVCtest.Models.book()
+                {
+                    ID = 01,
+                    Title = "Java for Dummies",
+                    Description = "Basics",
+                    Author = "Raymark",
+                    Publisher = "Raymark",
+                    DatePublished = "03/30/2020",
+                    BookFormat = "Book Bind",
+
+
+                };
+                return View(book);
+        }
+        public ActionResult List()
+        {
+            var book = new MVCtest.Models.book();
+            var list = new MVCtest.Models.book()
+            {
+                ID = book.ID,
+                Title = book.Title,
+                Description = book.Description,
+                Author = book.Author,
+                Publisher = book.Publisher,
+                DatePublished = book.DatePublished,
+                BookFormat = book.BookFormat,
+
+
+            };
+            return View(list);
+
+
+        }
+    }
+}
---
+++ book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCtest.Models
+{
+    public class book
+    {
+        [Required]
+        [Display(Name = "BOOK ID")]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "BOOK Title")]
+        public String Title { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public String Description { get; set; }
+        [Required]
+        [Display(Name = "Author")]
+        public String Author { get; set; }
+        [Required]
+        [Display(Name = "Publisher")]
+        public String Publisher { get; set; }
+        [Required]
+        [Display(Name = "Date Published")]
+        public String DatePublished { get; set; }
+        [Required]
+        [Display(Name = "Book Format")]
+        public String BookFormat { get; set; }
+
+
+    }
+}
